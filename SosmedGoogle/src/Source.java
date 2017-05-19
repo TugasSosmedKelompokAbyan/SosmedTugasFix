fef7775c51f2e4afdb72b85e77f8a8f9c258fbe4
Dell <Dell@Dala>
2017-05-19 18:40:03 +0700

    tambah

--- SosmedGoogle/src/Source.java
+++ SosmedGoogle/src/Source.java
@@ -5,4 +5,12 @@ public class Source {
 	    protected int tglLahir;
 	    protected String email;
 	    protected String jenisKelamin;
+	    
+	    public Data(String nama,int tglLahir, String email, String jenisKelamin) {
+	        this.nama = nama;
+	        this.tglLahir = tglLahir;
+	        this.email = email;
+	        this.jenisKelamin = jenisKelamin;
+	    }
+}
 }
