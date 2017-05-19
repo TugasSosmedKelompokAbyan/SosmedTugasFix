019d5173e142fc752feab2e00f1e34efdc21dc33
abyanfaishal <faishal.reza04@gmail.com>
2017-05-19 20:25:51 +0700

    program driver

---
+++ SosmedGoogle/src/DriverSosmed.java
@@ -0,0 +1,12 @@
+
+public class DriverSosmed {
+    public static void main(String[] args) {
+        ArrayList<Data> list = new ArrayList<>();
+        ArrayList<Data> list2 = new ArrayList<>();
+        HashMap<String, ArrayList> mapData = new HashMap<String, ArrayList>();
+        Scanner input = new Scanner(System.in);
+        
+        
+        String nama, email, jenisKelamin;
+        int mengulang, tglLahir;
+        int group;
